d3bb91a0dcdbf0a32e91f910631a8ac6d6f3f67d
sudo455 <aggelosmoraitis1@gmail.com>
2021-11-24 11:23:28 +0200

    Co-authored-by: Greekforce1821 <Greekforce1821@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#hello
+
+#https://discord.com/developers/docs/intro
+#https://vcokltfre.dev/tutorial/00-credits/
+#https://youtu.be/SPTfmiYiuok
+#https://discord.com/developers/applications/912839012136415294/information
+#https://discord.com/developers/applications/912835366866808875/information
+
+#OTEyODM5MDEyMTM2NDE1Mjk0.YZ1xlA._awvWEMIKF6lOts5Wgm--QzFQjE
+
+#https://discord.com/developers/docs/topics/oauth2#bots
+
